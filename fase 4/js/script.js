2963fa58aeb52419637cca338101826b3b992d3e
yeslyangarita <125774925+yeslyangarita@users.noreply.github.com>
2023-05-11 22:54:35 -0500

    fase 4

---
+++ fase 4/js/script.js
@@ -0,0 +1,26 @@
+function procesarrespuestas() {
+let total =5
+let puntos =0;
+
+let myForm = document.forms["formulario"];
+let respuesrascorrectas= ["a","d","a","b","c"];
+
+for (let i =1; i <= total; i++) {
+    if (myForm["r"+ i].value == null ||
+        myForm["r" +i].value == ''){
+        alert('Tienes que responder todas las preguntas');
+        return false;
+        } else {
+         if (myForm["r" + i].value === respuesrascorrectas[i - 1])
+         puntos++;  
+         
+        }    
+        
+        
+
+}
+let resultado = document.getElementById('resultado')
+resultado.innerHTML='Obtuviste  ' +puntos+ ' puntos de  ' + total + '  posibles ';
+return false;
+
+}
