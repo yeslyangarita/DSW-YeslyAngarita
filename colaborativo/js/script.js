7cea48961daa8fec26779263d6227ac1d9da6a39
yeslyangarita <125774925+yeslyangarita@users.noreply.github.com>
2023-05-12 00:45:58 -0500

    colaborativo

--- colaborativo/js/script.js
+++ colaborativo/js/script.js
@@ -24,3 +24,27 @@ function procesarrespuestas() {
     return false;
     
     }
+    function procesarRespuestas(){
+        let total = 5;
+        let puntos = 0;
+    
+        let myForm = document.forms["formulario"];
+        let respuestasCorrectas = ["c", "a", "d", "b", "c"];
+    
+        for(let i = 1; i<= total; i++){
+            if(myForm["r"+i].value == null || myForm["r"+i].value == ''){
+                alert('Favor responder todas las pregutas');
+                window.location.reload()
+                return false;
+            }else{
+                if(myForm["r"+i].value === respuestasCorrectas[i - 1]){
+                    puntos++;
+                }
+            }
+        }
+    
+        let resultado = document.getElementById('resultado');
+    
+        resultado.innerHTML = 'Obtuviste ' + puntos + ' puntos de ' + total + ' posibles ';
+        return false;
+    }
